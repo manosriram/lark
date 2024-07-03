3bb3410127fb73a06d8895493550af0dee9539b7
Mano Sriram <mano.sriram0@gmail.com>
2024-07-04 02:50:00 +0530

    fix: fix precedence of expression parsing

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -6,53 +6,6 @@ import (
 	"unicode"
 )
 
-type TOKEN_TYPE string
-type STATEMENT_TYPE int
-type LITERAL_TYPE int
-
-const (
-	EXPRESSION_STATEMENT STATEMENT_TYPE = iota
-	ASSIGN_STATEMENT
-)
-
-const (
-	FLOAT64 LITERAL_TYPE = iota
-	INTEGER
-	STRING
-)
-
-const (
-	PLUS      TOKEN_TYPE = "+"
-	MINUS                = "-"
-	MULTIPLY             = "*"
-	DIVIDE               = "/"
-	LBRACE               = "("
-	RBRACE               = ")"
-	EQUAL                = "="
-	NOT                  = "!"
-	NOT_EQUAL            = "!="
-	SEMICOLON            = ";"
-	ID                   = "id"
-	LITERAL              = "literal"
-	EOF                  = "EOF"
-	EXPR                 = "expr"
-	DOT                  = "."
-)
-
-type Token struct {
-	Position    int
-	TokenType   TOKEN_TYPE
-	Value       interface{}
-	LiteralType LITERAL_TYPE
-}
-
-type Source struct {
-	Content         string
-	Tokens          []Token
-	CurrentPosition int
-	TokenSoFar      string
-}
-
 func (s *Source) getCurrentToken() byte {
 	return s.Content[s.CurrentPosition]
 }
